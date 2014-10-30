a38d25a6fc61d93afd1402978219f0c30de9d0cc
Alexander Simeonov <agsimeon@buffalo.edu>
2014-10-30 01:44:21 -0400

    SwapVector Musings

---
+++ SwapApp/src/edu/buffalo/swapapp/SwapVector.java
@@ -0,0 +1,35 @@
+package edu.buffalo.swapapp;
+
+import java.io.Serializable;
+import java.util.AbstractList;
+import java.util.List;
+import java.util.RandomAccess;
+
+public class SwapVector<E extends Serializable> extends AbstractList<E> 
+                                                implements List<E>,
+                                                           RandomAccess, 
+                                                           Cloneable, 
+                                                           java.io.Serializable {
+	protected int capacityIncrement;
+	private static final long serialVersionUID = -8062241491045229885L;
+    
+    public SwapVector(int initialCapacity, int capacityIncrement) {
+        super();
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal Capacity: "+ initialCapacity);
+        // Create file of given initialCapacity perhaps?
+        this.capacityIncrement = capacityIncrement;
+    }
+
+    @Override
+    public E get(int location) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
