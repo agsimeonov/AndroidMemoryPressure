60f0bca0be44cc03cbcbf0f0c8a1f0513ec752b6
Alexander Simeonov <agsimeon@buffalo.edu>
2014-11-04 01:39:13 -0500

    Added some memory information tools

--- SwapApp/src/edu/buffalo/swapapp/MainActivity.java
+++ SwapApp/src/edu/buffalo/swapapp/MainActivity.java
@@ -1,12 +1,60 @@
 package edu.buffalo.swapapp;
 
+import java.io.File;
+import java.util.Scanner;
+
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showAvailableMemory();
+    }
+    
+    private void showAvailableMemory() {
+        ActivityManager activityManager =  (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        MemoryInfo memoryInfo = new MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        Log.i("memory footprint", "" + memoryInfo.availMem / memoryInfo.totalMem + "%");
+        Log.i("TOTAL MEMORY", "" + (MemInfo.getTotalMem() >> 20));
+        Log.i("AVAILABLE MEMORY", "" + (MemInfo.getAvailableMem(this) >> 20));
+        Log.i("FREE MEMORY", "" + (MemInfo.getFreeMem() >> 20));
+        Log.i("THRESHOLD MEMORY", "" + (memoryInfo.threshold >> 20));
+        
+
+        int pid = android.os.Process.myPid();
+        Log.i("memory footprint", "" + (scanProcForField("/proc/"+pid+"/status", "VmSize") >>20));
+
+
+        Log.i("memory footprint", "" + (MemInfo.getMaxHeap() >>20));
+//        Log.i("memory footprint", "" + scanProcForField("/proc/"+pid+"/status", "VmSize"));
+        
+    }
+    
+    private static long scanProcForField(String path, String field) {
+        File file = new File(path);
+        Scanner scanner = null;
+        field += ":";
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String token = scanner.next();
+                if (token.equals(field))
+                    return scanner.nextLong() << 10;  // It's in KB...
+                String s = scanner.nextLine();
+            }
+        } catch (Exception e) {
+            // Should we propagate this? Fall through for now.
+            Log.e("MemUtil", "Error scanning "+file+" for "+field, e);
+        } finally {
+            if (scanner != null) scanner.close();
+        } return -1;
+    }
 }
---
+++ SwapApp/src/edu/buffalo/swapapp/MemInfo.java
@@ -0,0 +1,66 @@
+package edu.buffalo.swapapp;
+
+import java.io.File;
+import java.util.Scanner;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.MemoryInfo;
+import android.content.Context;
+import android.util.Log;
+
+public final class MemInfo extends Activity {
+    
+    public static long getMaxHeap() {
+        return Runtime.getRuntime().maxMemory();
+    }
+    
+    public static long getFreeHeap() {
+    	return Runtime.getRuntime().freeMemory();
+    }
+    
+    public static long getTotalMem() {
+    	return scanProcForField("/proc/meminfo", "MemTotal");
+    }
+    
+    public static long getFreeMem() {
+    	return scanProcForField("/proc/meminfo", "MemFree");
+    }
+    
+    public static long getAvailableMem(Activity activity) {
+        ActivityManager activityManager =  (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
+        MemoryInfo memoryInfo = new MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        return memoryInfo.availMem;
+    }
+    
+    public static long getThreshold(Activity activity) {
+        ActivityManager activityManager =  (ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE);
+        MemoryInfo memoryInfo = new MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+        return memoryInfo.threshold;
+    }
+    
+    private static long scanProcForField(String path, String field) {
+        File file = new File(path);
+        Scanner scanner = null;
+        field += ":";
+
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String token = scanner.next();
+                if (token.equals(field))
+                    return scanner.nextLong() << 10; // It's in KB...
+                scanner.nextLine();
+            }
+        } catch (Exception e) {
+            // Should we propagate this? Fall through for now.
+            Log.e("MemInfo", "Error scanning " + file + " for " + field, e);
+        } finally {
+            if (scanner != null)
+                scanner.close();
+        }
+        return -1;
+    }
+}
